e375a809b5a74224de6667632a5282bfb492ac88
kwasikwarteng <megakwasikwarteng@gmail.com>
2023-11-26 13:58:48 +0000

    Model and Database Configuration

---
+++ PhoneBookApp/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBookApp.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
